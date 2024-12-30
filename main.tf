e30c7ea3bc7b70bcff4a3070d6c12203f9d4d502
shilpak27 <shilpakjoshi27@gmail.com>
2024-12-30 20:33:18 +0530

    f4

--- main.tf
+++ main.tf
@@ -1,29 +1,11 @@
-provider "aws" {
-  region = "us-east-1" # Change to your desired region
-}
-
-resource "aws_instance" "instance_1" {
-  ami           = "ami-0fd05997b4dff7aac" # Replace with a valid AMI ID for your region
-  instance_type = "t2.micro"              # Change to your desired instance type
+resource "aws_instance" "my-ec2" {
+   ami = "ami-0fd05997b4dff7aac"
+  instance_type = "t2.micro"
+  count = 2
 
   tags = {
-    Name = "Instance-1"
+    Name = "my-ec2-machines"
+
+    
   }
-}
-
-resource "aws_instance" "instance_2" {
-  ami           = "ami-0fd05997b4dff7aac" # Replace with a valid AMI ID for your region
-  instance_type = "t2.micro"              # Change to your desired instance type
-
-  tags = {
-    Name = "Instance-2"
-  }
-}
-
-output "instance_1_public_ip" {
-  value = aws_instance.instance_1.public_ip
-}
-
-output "instance_2_public_ip" {
-  value = aws_instance.instance_2.public_ip
 }
